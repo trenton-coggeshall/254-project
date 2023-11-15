3a48485df3246f0da359b3ba71525631d0e99489
trenton-coggeshall <90164941+trenton-coggeshall@users.noreply.github.com>
2023-11-14 17:34:02 -0800

    Added basic calendar

--- 254-project/src/App.js
+++ 254-project/src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Calendar, { MonthView } from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [value, setValue] = useState(new Date());
+
+  useEffect(() => {
+    console.log(value);
+  }, [value]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Calendar 
+        onChange={setValue}
+        value={value}
+        />
       </header>
     </div>
   );
